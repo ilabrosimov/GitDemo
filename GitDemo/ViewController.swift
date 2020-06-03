6ba4240b3e0f0c6f9d3bc2db41e3d8408d310255
ilabrosimov <ilabrosimov@gmail.com>
2020-06-03 13:07:01 +0300

    New line in NewFeatureLog

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         print("Hello world")
+        print("New feature branch")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
